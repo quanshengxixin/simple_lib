9d0777f63f57990f7d5805328db7801c737d7c0e
quanshengxixin <quanshengxixin@outlook.com>
2020-11-28 22:08:31 +0800

    add container_of function
    Signed-off-by: quanshengxixin <quanshengxixin@outlook.com>

--- include/simple_kernel.h
+++ include/simple_kernel.h
@@ -96,6 +96,9 @@
 #define container_of(ptr, type, member) ({			        \
 	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
 	(type *)( (char *)__mptr - offsetof(type,member) );})
-
+#if 0
+#define container_of(ptr, type, member)                \
+        (type *)((char *)(ptr) - offsetof(type, member))
+#endif
 
 #endif // __SIMPLE_KERNEL_H__
